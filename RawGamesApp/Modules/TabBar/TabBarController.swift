c98785e34df5b067131864a6da0a826ac65b11b6
Ibrohim <ibrohim625m@gmail.com>
2023-11-09 17:03:43 +0500

    Add `TabBarController` UI page

---
+++ RawGamesApp/Modules/TabBar/TabBarController.swift
@@ -0,0 +1,57 @@
+//
+//  TabBarController.swift
+//  RawGamesApp
+//
+//  Created by Ibrokhim Movlonov on 09/11/23.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController, UITabBarControllerDelegate {
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        delegate = self
+        setupControllers()
+    }
+    
+    // MARK: - Private
+    private func setupControllers() {
+        let homeCtr = getControllerAt(type: .home)
+        let favoriteCtr = getControllerAt(type: .favorite)
+        
+        setViewControllers([homeCtr, favoriteCtr], animated: false)
+        tabBar.backgroundColor = .white
+    }
+    
+    private func getControllerAt(type: TabBarItemType) -> UIViewController {
+        switch type {
+        case .home:
+            let home = UIViewController()
+            return createTabBarItem(ctr: home, type: type)
+            
+        case .favorite:
+            let favorite = UIViewController()
+            return createTabBarItem(ctr: favorite, type: type)
+            
+        case .unknown:
+            return UIViewController()
+        }
+    }
+    
+    private func createTabBarItem(ctr: UIViewController, type: TabBarItemType) -> UINavigationController {
+        let nv = UINavigationController(rootViewController: ctr)
+        let customTab = TabBarItem(
+            title: type.name.localized(),
+            image: UIImage(named: type.icon),
+            selectedImage: UIImage(named: type.icon))
+        customTab.tabItemType = type
+        customTab.setTitleTextAttributes([NSAttributedString.Key.font: UIFont.systemFont(ofSize: 10)], for: .normal)
+        nv.tabBarItem = customTab
+        
+        return nv
+    }
+
+}
