3b3e016d7f1caf62284dfe875295e881249252e0
Ibrohim <ibrohim625m@gmail.com>
2023-11-11 15:27:43 +0500

    Create URLRequest+Init helper file

--- RawGamesApp/Api Service/HTTPClient.swift
+++ RawGamesApp/Api Service/HTTPClient.swift
@@ -14,3 +14,29 @@ public protocol HTTPClient {
     /// Client are responsible to dispatch to appropriate thread, if needed.
     func get(with request: URLRequest, completion: @escaping (Result) -> Void)
 }
+
+extension HTTPClient {
+    /** Overloaded request - Gets URL and Completion as params  */
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+        get(from: url, method: .GET, completion: completion)
+    }
+
+    /** Overloaded request - Gets URL, http method and Completion as params  */
+    func get(from url: URL, method: HTTPMethod, completion: @escaping (HTTPClient.Result) -> Void) {
+        get(from: url, method: method, headers: nil, body: nil, completion: completion)
+    }
+    
+    /** Overloaded request - Gets URL, http method, headers, body dictionary and Completion as params  */
+    func get(from url: URL, method: HTTPMethod, headers: [String: String]?, body: [String: String]? = nil, completion: @escaping (Result) -> Void) {
+        let request = buildRequest(from: url, method: method, headers: headers, bodyDictionary: body)
+        get(with: request, completion: completion)
+    }
+}
+
+private extension HTTPClient {
+    func buildRequest(from url: URL, method: HTTPMethod = .GET, headers: [String: String]? = nil, bodyDictionary: [String: String]? = nil) -> URLRequest {
+        let request = URLRequest.builder(url: url, method: method, headers: headers, bodyMap: bodyDictionary)
+
+         return request
+     }
+}
