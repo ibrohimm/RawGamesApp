18e57d6f72e67f66e5fc2c3b2ba8717ca3092dca
Ibrohim <ibrohim625m@gmail.com>
2023-11-11 12:03:48 +0500

    Create `HTTPClient` protocol

---
+++ RawGamesApp/Api Service/HTTPClient.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPClient.swift
+//  RawGamesApp
+//
+//  Created by Ibrokhim Movlonov on 11/11/23.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    typealias Result = Swift.Result<Data, Error>
+    
+    /// The completion handler can be invoked in any thread.
+    /// Client are responsible to dispatch to appropriate thread, if needed.
+    func get(with request: URLRequest, completion: @escaping (Result) -> Void)
+}
