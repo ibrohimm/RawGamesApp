4b55e5862c3ef71ad5479d91d9dea6a3c91cf495
Ibrohim <ibrohim625m@gmail.com>
2023-11-13 00:06:22 +0500

    Add GameDetail struct to use as data transfer object

---
+++ RawGamesApp/Modules/Detail/Model/GameDetail.swift
@@ -0,0 +1,20 @@
+//
+//  GameDetail.swift
+//  RawGamesApp
+//
+//  Created by Ibrokhim Movlonov on 12/11/23.
+//
+
+import Foundation
+
+struct GameDetail {
+    let id: Int
+    let name, gameDetailModelDescription: String
+    let backgroundImage: String?
+    let added: Int
+    let rating: Double
+    let ratingTop: Int?
+    let released: String?
+    let reviewsCount: Int
+    let website: String?
+}
