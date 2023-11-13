6478618046ac82b5f6c899f416396b1161d02434
Ibrohim <ibrohim625m@gmail.com>
2023-11-13 08:52:38 +0500

    Add tableView to detail screen

--- RawGamesApp/Modules/Detail/DetailViewController.swift
+++ RawGamesApp/Modules/Detail/DetailViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 final class DetailViewController: UIViewController, DetailViewInput {
     
     private var presenter: DetailViewOutput?
+    private let tableView = UITableView(frame: .zero, style: .grouped)
     
     // MARK: - Life Cycle
     
@@ -25,11 +26,48 @@ final class DetailViewController: UIViewController, DetailViewInput {
         self.presenter = presenter
     }
     
-    // MARK: - UI Setup
-    
-    private func setupUI() {
-    }
-    
     // MARK: - View Input
     
+    func reloadTableView() {
+        
+    }
+}
+
+// MARK: -
+extension DetailViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { 1 }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+// MARK: - UI Setup
+extension DetailViewController {
+    private func setupUI() {
+        view.backgroundColor = .appColor(.primaryBackground)
+        createTableView()
+        
+        let ratingLabel = UILabel()
+        ratingLabel.text = "Favorite"
+        ratingLabel.textColor = .gray
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: ratingLabel)
+    }
+    
+    private func createTableView() {
+        tableView.backgroundColor = .clear
+        tableView.separatorStyle = .singleLine
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
