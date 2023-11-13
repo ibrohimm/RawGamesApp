b4e03e1fe1f1543265a389cd96a7bda060f832f9
Ibrohim <ibrohim625m@gmail.com>
2023-11-13 09:10:01 +0500

    Add DetailSections enum to show the cell's sections

---
+++ RawGamesApp/Modules/Detail/Helper/DetailSections.swift
@@ -0,0 +1,13 @@
+//
+//  DetailSections.swift
+//  RawGamesApp
+//
+//  Created by Ibrokhim Movlonov on 13/11/23.
+//
+
+import Foundation
+
+enum DetailSections: Int, CaseIterable {
+    case image
+    case description
+}
