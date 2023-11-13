b591d9af795f9ad52985d818b59c10a73c44c5be
Ibrohim <ibrohim625m@gmail.com>
2023-11-13 15:27:04 +0500

    Add Favorite View Model

---
+++ RawGamesApp/Modules/Favorite/ViewModel/FavoriteViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteViewModel.swift
+//  RawGamesApp
+//
+//  Created by Ibrokhim Movlonov on 13/11/23.
+//
+
+import Foundation
+
+struct FavoriteViewModel {
+    let id: Int
+    let name: String
+    let gameDetailModelDescription: String
+    let backgroundImage: String?
+    let website: String?
+}
